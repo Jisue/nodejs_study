415eab965d13eda2631f6b597ecc8af65df55992
jisue kim <jisue0216@gmail.com>
2020-10-28 19:26:26 +0900

    add day3
    파일 읽기 까지

--- main.js
+++ main.js
@@ -5,40 +5,66 @@ var url = require('url');
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
-    if(_url == '/'){
-      title = 'Welcome';
-    }
-    if(_url == '/favicon.ico'){
-      return response.writeHead(404);
-    }
-    response.writeHead(200);
-    // main.js가 위치하는 디렉토리 출력됨.
-    // /index.html이 전달됨.
+    var pathname = url.parse(_url, true).pathname;
 
-    //querystring에따라서 파일명 들어감.
-    fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
-        <h2>${title}</h2>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
-      response.end(template);
-    })
+    //console.log(url.parse(_url, true).pathname);
 
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = 'Welcome';
+          var description = 'Hello, Node.js';
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var template = `
+          <!doctype html>
+          <html>
+          <head>
+            <title>WEB1 - ${title}</title>
+            <meta charset="utf-8">
+          </head>
+          <body>
+            <h1><a href="/">WEB</a></h1>
+            <ul>
+              <li><a href="/?id=HTML">HTML</a></li>
+              <li><a href="/?id=CSS">CSS</a></li>
+              <li><a href="/?id=JavaScript">JavaScript</a></li>
+            </ul>
+            <h2>${title}</h2>
+            <p>${description}</p>
+          </body>
+          </html>
+          `;
+          response.writeHead(200); // 웹서버와 웹브라우저 사이에서 잘 됐는지 통신하기 위한 약속,성공
+          response.end(template);
+        });
+      }
+    } else {
+      response.writeHead(404); // 파일을 찾을 수 없는 경우
+      response.end('Not found');
+    }
 });
 app.listen(3000);
